8557cee9555c6724af2d962e5bc59ecdb0af2970
Md. Heridoy Ahammed <91461922+heridoy@users.noreply.github.com>
2021-10-25 17:07:49 +0600

    day3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:hridoy_app/homepage.dart';
 import 'package:hridoy_app/loginpage.dart';
 
+import 'Routes.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.amber),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
